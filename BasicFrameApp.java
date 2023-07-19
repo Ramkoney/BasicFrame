1c02f8a2599a045460c81fa5274ba5b3cd3e3fe3
Ramkoney <140020922+Ramkoney@users.noreply.github.com>
2023-07-19 23:38:57 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicframeapp;
+
+import za.ac.tut.us.MyFirstFrame;
+
+/**
+ *
+ * @author MJ Mokgosana
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    MyFirstFrame myFrame;
+    myFrame = new MyFirstFrame();
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.us;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author MJ Mokgosana
+ */
+public class MyFirstFrame extends JFrame{
+
+    public MyFirstFrame() {
+        setTitle("My first GUI");
+        setSize(200,250);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+        
+        
+    }
+    
+}
